a5067a878bb453204600200b56fab0ee8d4dddfe
ShreenathR7 <141302681+ShreenathR7@users.noreply.github.com>
2023-08-25 18:20:36 +0530

    Add files via upload

---
+++ Bulk_Tagging/Test_data/data.py
@@ -0,0 +1,18 @@
+# data.py - Keep all your Testing Data in this file
+class Logi_Data:
+    url = 'https://retail.logimaxindia.com/etail_v3/admin/index.php/customer/add'
+
+    username = 'logimax'
+
+    password = 'lmx#dev@etail'
+    
+    id  = 'GB-00001'
+    
+    charge = '400'
+    
+    old_id = 'GC-20001'
+    
+    MRP_Code = 'MRP-00002'
+    
+    rate = '80000'
+    
---
+++ Bulk_Tagging/Test_locators/locators.py
@@ -0,0 +1,81 @@
+# locators.py - File where all The HTML Locators are Kept
+class Logi_Locators:
+    username_inputBox = 'username'
+
+    password_inputBox = 'password'
+     
+    signButton ='(//*[@id="submit_login"])'
+    
+    side_bar = '(//*[@role="button"])'
+    
+    retail_catalog = '(//*[@class="fa fa-angle-left pull-right"])[3]'
+   
+    Inventory_module = 'Inventory'
+    
+    Bulk_Tagging= 'Bulk Tag Edit'
+    
+    choose_file = 'bulk_edit_options'
+    
+    select_branch = '(//*[@role="presentation"])[2]'
+    
+    text_box = '(//*[@role="textbox"])'
+    
+    select_product = '(//*[@role="presentation"])[4]'
+    
+    select_design = '(//*[@role="presentation"])[6]'
+    
+    select_sub_design = '(//*[@role="presentation"])[8]'
+ 
+    Add_filter = "get_tag_details"
+    
+    update_mc_type = 'update_mc_type'
+    
+    mc_value = 'mc_value'
+    
+    submit = 'otp_submit'
+    
+    search = '(//*[@type="search"])'
+    
+    wastage_percent = 'wastage_percent'
+    
+    Tag_code = 'be_tag_code'
+    
+    pcs = 'tagging[pcs]'
+    
+    add_attribute = 'update_tag_add_attribute'
+    
+    select_attribute = '(//*[@role="presentation"])[12]'
+    
+    list_attributes = '(//*[@role="presentation"])[13]'
+    
+    Attribute_edit = 'attribute_type'
+    
+    add_image = '(//*[@class="btn btn-default btn-sm"])'
+    
+    image_file = 'bulktag_images'
+    
+    save_button = 'update_img'
+
+    image_eye = '(//*[@class="bi bi-eye"])[2]'
+    
+    is_default = '(//*[@class="tag_default_0"])'
+    
+    delete_image = 'Delete'
+    
+    imagepreview_save = 'update_img_bulk'
+    
+    add_charge = 'update_update_add_charges'
+    
+    charge_type = '(//*[@class="form-control bulk_tag_upd_charge_name"])'
+    
+    value = '(//*[@class="form-control bulk_tag_upd_charge_value"])'
+    
+    old_tag_id = 'blk_old_tag'
+    
+    MRP_rate = 'be_mrp_value'
+    
+    size = "tagging[size]"
+    
+    
+    
+    
